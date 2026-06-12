71a9e437b0be36ad650191cce6f50ede46cff6ca
Hai Nam <hainamwork23@gmail.com>
2026-06-12 21:51:33 +0700

    Complete the draft front page
    Add another components and change the App.tsx to meet the same style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css';
 import { Carousel } from './layouts/Homepage/Carousel';
 import { ExploreBooks } from './layouts/Homepage/ExploreBook';
+import { Guidance } from './layouts/Homepage/Guidance';
+import { SearchService } from './layouts/Homepage/SearchService';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
-function App() {
+export const App = () =>  {
   return (
       <div>
         <Navbar/>
         <ExploreBooks/>
         <Carousel/>
+        <Guidance/>
+        <SearchService/>
+        <Footer/>
       </div>
      
   );
 }
 
-export default App;
